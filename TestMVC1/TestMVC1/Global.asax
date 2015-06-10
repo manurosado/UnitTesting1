9908b4237f8b4d68f2a3270240934d537db4bf73
manurosado <emmanuel.rosado@outlook.com>
2015-06-10 17:26:19 -0400

    Testing  Check In

---
+++ TestMVC1/TestMVC1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestMVC1.MvcApplication" Language="C#" %>
---
+++ TestMVC1/TestMVC1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestMVC1.Startup))]
+namespace TestMVC1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
