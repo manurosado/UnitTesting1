e0bc90b52b5ebd69f8f0da935f5bf422f82697ca
manurosado <emmanuel.rosado@outlook.com>
2015-06-10 22:12:31 -0400

    Added ionic first app

---
+++ BlankCordovaApp1/BlankCordovaApp1/scripts/app.js
@@ -0,0 +1,8 @@
+﻿var App = angular.module("App", ["ionic"]);
+
+App.controller("AppCtrl", ["@scope", "$log", AppCtrl]);
+function AppCtrl($scope, $log) {
+    $scope.refresh = function () {
+        alert("button pressed");
+    }
+}
